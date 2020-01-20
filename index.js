c03b6b5bca9d130349e04f4784c54b7916ec23b0
raiden808 <raiden.nalasa@gmail.com>
2020-01-20 08:31:14 +0800

    rest update delete

--- index.js
+++ index.js
@@ -85,10 +85,11 @@ app.get('/api/persons/:id',(request,response)=>{
  * Person delete function
  */
 app.delete('/api/persons/:id',(request,response)=>{
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result =>{
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 });
 
 /**
